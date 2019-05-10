d1a8777b41cc66619ecccac741f5d2312a4f2b76
Tom Reed <t.reed1104@gmail.com>
2019-05-11 00:03:36 +0100

    Comment edits

--- components/vertex_in.glsl
+++ components/vertex_in.glsl
@@ -1,4 +1,4 @@
-// Generic In variables for a vertex shader, these are normally values from an object mesh
+// Generic In variables for a vertex shader
 in vec3 vertexPosition;
 in vec3 vertexColor;
 in vec2 vertexUV;
