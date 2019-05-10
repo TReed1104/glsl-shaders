f35be4fc70975e021dcd9d714bcc181c6ffc4244
Tom Reed <t.reed1104@gmail.com>
2019-05-11 00:05:42 +0100

    Amended lighting uniforms ready for specular lighting

--- components/lighting_uniforms.glsl
+++ components/lighting_uniforms.glsl
@@ -1,4 +1,6 @@
 // Lighting uniforms
 uniform vec3 lightingColor;
-uniform vec3 lightingPosition
-uniform float ambientStrength;  // How strong is the ambient lighting?
+uniform vec3 lightingPosition;
+uniform vec3 cameraPosition;
+uniform float ambientStrength;      // How strong is the ambient lighting?
+uniform float specularStrength;     // How intense if the specular lighting?
