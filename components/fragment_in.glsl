8d4c6bca4885eb19d82a39a2fefd114872e9f524
Tom Reed <t.reed1104@gmail.com>
2019-05-11 00:08:11 +0100

    Amended the fragment values to all be named consistently

--- components/fragment_in.glsl
+++ components/fragment_in.glsl
@@ -1,5 +1,5 @@
 // Generic In variables for a fragment shader
 in vec3 fragmentPosition;
 in vec3 fragmentColour;
-in vec2 UV;
-in vec3 normal;
+in vec2 fragmentUV;
+in vec3 fragmentNormal;
--- components/vertex_out.glsl
+++ components/vertex_out.glsl
@@ -1,5 +1,5 @@
 // Generic Out variables for a vertex shader, these are the values passed to the fragment shader
 out vec3 fragmentPosition;
 out vec3 fragmentColour;
-out vec2 UV;
-out vec3 normal;
+out vec2 fragmentUV;
+out vec3 fragmentNormal;
