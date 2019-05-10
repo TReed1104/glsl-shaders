19e0f82e5e8218bb8247a5da7b955241b3147eec
Tom Reed <t.reed1104@gmail.com>
2019-05-11 00:02:03 +0100

    Added fragmentPosition in/out

--- components/fragment_in.glsl
+++ components/fragment_in.glsl
@@ -1,4 +1,5 @@
-// Generic In variables for a fragment shader (mesh colour and TexCoords)
+// Generic In variables for a fragment shader
+in vec3 fragmentPosition;
 in vec3 fragmentColour;
 in vec2 UV;
 in vec3 normal;
--- components/vertex_out.glsl
+++ components/vertex_out.glsl
@@ -1,4 +1,5 @@
 // Generic Out variables for a vertex shader, these are the values passed to the fragment shader
+out vec3 fragmentPosition;
 out vec3 fragmentColour;
 out vec2 UV;
 out vec3 normal;
