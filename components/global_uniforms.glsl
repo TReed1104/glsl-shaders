e0a6c0bebb791fa8a45d0c0e1b6fe771ba45b7f1
Tom Reed <t.reed1104@gmail.com>
2019-05-12 16:49:06 +0100

    Moved the cameraPosition uniform to the global uniforms

--- components/global_uniforms.glsl
+++ components/global_uniforms.glsl
@@ -2,3 +2,4 @@
 uniform vec3 iResolution;
 uniform float iTime;
 uniform vec4 iMouse;
+uniform vec3 iCameraPosition;
--- components/lighting_uniforms.glsl
+++ components/lighting_uniforms.glsl
@@ -1,6 +1,5 @@
 // Lighting uniforms
 uniform vec3 lightingColor;
 uniform vec3 lightingPosition;
-uniform vec3 cameraPosition;
 uniform float ambientStrength;      // How strong is the ambient lighting?
 uniform float specularStrength;     // How intense if the specular lighting?
