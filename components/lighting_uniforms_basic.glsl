2402f204c0eec13c1930e75e7ebdd80ac73d758a
Tom Reed <t.reed1104@gmail.com>
2019-05-16 11:47:26 +0100

    Removed the positional lighting shader as its now completely defunct

--- components/lighting_uniforms_basic.glsl
+++
@@ -1,5 +0,0 @@
-// Lighting uniforms
-uniform vec3 lightingColor;
-uniform vec3 lightingPosition;
-uniform float ambientStrength;      // How strong is the ambient lighting?
-uniform float specularStrength;     // How intense if the specular lighting?
