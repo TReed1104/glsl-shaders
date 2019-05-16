2ef5c5244fca9b119d977b3ad2c86bd05a5a24fb
Tom Reed <t.reed1104@gmail.com>
2019-05-16 15:34:12 +0100

    spotlighting renames and comments

--- components/lighting_uniforms.glsl
+++ components/lighting_uniforms.glsl
@@ -1,15 +1,17 @@
-// Object holding all of a light's attributes
+// Light Struct, encapsulates the light attributes
 struct Light {
     // General lighting attributes
     vec3 position; 
     vec3 direction;
+
+    // Colours and intensities
     vec3 ambientIntensity;
     vec3 diffuseColour;
     vec3 specularIntensity;
 
     // Spotlighting
-    float cutOff;
-    float outerCutOff;
+    float spotlightCutOff;
+    float spotlightCutOffOuter;
     
     // Attenuation
     float attenuationConstant;
