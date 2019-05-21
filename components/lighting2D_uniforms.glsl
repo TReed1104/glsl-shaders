3d5bb41c09f77566c7dd4d5f2b849bca169a8a1b
Tom Reed <t.reed1104@gmail.com>
2019-05-21 10:59:43 +0100

    Fixed an error in the light2D component

--- components/lighting2D_uniforms.glsl
+++ components/lighting2D_uniforms.glsl
@@ -4,10 +4,11 @@ struct Light2D {
     vec2 direction;     // used for spotlighting
 
     vec3 colour;
-    float intensity;    // How far can the light reach?
+    float ambientIntensity;
+    float attenuation;    // How far can the light reach?
     
     float spotlightAngle;
     float spotlightCutOff;
-}
+};
 
 uniform Light2D light;
